0c87cfe4e6531aa568aabc88b371d7185c4b5fe5
FelipeDePaula-Developer <felipe-dev@outlook.com>
2022-06-27 00:24:12 -0300

    Ajuste na query que busca o registro do login

--- src/main/java/com/services/syslogin/repository/UserLoginRepository.java
+++ src/main/java/com/services/syslogin/repository/UserLoginRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserLoginRepository extends CrudRepository<UserLogin, Integer> {
-    @Query(value = "select ul from user_login ul where ul.user_key = ?1 and ul.logged = 'T' and ul.user = ?2")
+    @Query(value = "select ul from user_login ul where ul.user_key = ?1 and ul.logged = 'T' and ul.user = ?2 and ul.remember_me = 'T'")
     UserLogin getUserLoginByUser_Key(String key, String userID);
 }
